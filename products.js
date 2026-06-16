ae065149faa712fc8e7739b84fc46fb24a774218
harsh-anuj <harshgrawa543@gmail.com>
2026-06-16 18:20:22 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,24 @@
+const products = [
+
+{
+name: "Premium Watch",
+price: 1999,
+category: "Accessories",
+image: "https://via.placeholder.com/250"
+},
+
+{
+name: "Wireless Earbuds",
+price: 2999,
+category: "Electronics",
+image: "https://via.placeholder.com/250"
+},
+
+{
+name: "Fashion Hoodie",
+price: 1499,
+category: "Fashion",
+image: "https://via.placeholder.com/250"
+}
+
+];
