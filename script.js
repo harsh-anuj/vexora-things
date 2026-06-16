f2080cd9147264437aba9d0530bb4adb16c919c7
harsh-anuj <harshgrawa543@gmail.com>
2026-06-16 17:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let cart = [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function addToCart(productName, price) {
+
     cart.push({
         name: productName,
         price: price
     });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
 
     alert(productName + " added to cart!");
 }
